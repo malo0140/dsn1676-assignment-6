a4fe34127964129620f4f15d23175841170e279b
malo0140 <karamaloney@Karas-MacBook-Pro.local>
2015-03-14 14:23:29 -0400

    First button

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var $showHide = $('.btn-show-hide');
+var $box = $('.box');
+
+var $btnMove = $('.btn-move');
+var $dimond = $('.dimond');
+
+$showHide.on('click', function () {
+    $box.toggleClass('js-box-show');
+});
