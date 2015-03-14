2eb9a171012efd41f78fb92156d2a8be4e768245
malo0140 <karamaloney@Karas-MacBook-Pro.local>
2015-03-14 15:25:12 -0400

    bounce added

--- js/main.js
+++ js/main.js
@@ -7,6 +7,9 @@ var $diamond = $('.diamond');
 var $btnCollapse = $('.btn-collapse-expand');
 var $panel = $('.panel');
 
+var $btnBounce = $('.btn-bounce');
+var $Circle = $('.circle');
+
 $showHide.on('click', function () {
     $box.toggleClass('js-box-show');
 });
@@ -19,4 +22,7 @@ $btnCollapse.on('click', function () {
     $panel.toggleClass('js-panel-collapse');
 });
 
+$btnBounce.on('click', function () {
+    $Circle.toggleClass('js-ball-bounce');   
+});
 
